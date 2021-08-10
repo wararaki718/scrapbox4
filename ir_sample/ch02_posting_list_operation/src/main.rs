da15d73e11a88068924545e24a90206e363e7b10
wararaki <ky7.ott.w@gmail.com>
2021-08-10 22:11:26 +0900

    update

--- ir_sample/ch02_posting_list_operation/src/main.rs
+++ ir_sample/ch02_posting_list_operation/src/main.rs
@@ -1,9 +1,14 @@
 use std::collections::HashMap;
 
+
+static INF: i32 = i32::MAX;
+static NEG_INF: i32 = i32::MIN;
+
+
 fn get_posting_list() -> HashMap<String, Vec<i32>> {
-    let v = vec![11, 22, 33];
-    let v2 = vec![44, 55, 66];
-    let v3 = vec![77, 88, 99];
+    let v = vec![NEG_INF, 11, 22, 33, INF];
+    let v2 = vec![NEG_INF, 44, 55, 66, INF];
+    let v3 = vec![NEG_INF, 77, 88, 99, INF];
     let mut posting_list = HashMap::new();
     posting_list.insert("first".to_string(), v);
     posting_list.insert("second".to_string(), v2);
@@ -23,13 +28,20 @@ fn first(term: String, posting_list: &HashMap<String, Vec<i32>>) -> i32 {
 fn last(term: String, posting_list: &HashMap<String, Vec<i32>>) -> i32 {
     if posting_list.contains_key(&term) {
         let indices = &posting_list[&term];
-        let length = posting_list.len();
+        let length = indices.len();
         return indices[length-1];
     }
     return -1;
 }
 
 fn next(term: String, posting_index: i32, posting_list: &HashMap<String, Vec<i32>>) -> i32 {
+    if posting_index == INF {
+        return INF;
+    }
+    if posting_index == NEG_INF {
+        return first(term, &posting_list);
+    }
+
     if posting_list.contains_key(&term) {
         let indices = &posting_list[&term];
         let index = indices.binary_search(&posting_index).unwrap();
@@ -42,10 +54,16 @@ fn next(term: String, posting_index: i32, posting_list: &HashMap<String, Vec<i32
 }
 
 fn prev(term: String, posting_index: i32, posting_list: &HashMap<String, Vec<i32>>) -> i32 {
+    if posting_index == NEG_INF {
+        return NEG_INF;
+    }
+    if posting_index == INF {
+        return last(term, &posting_list);
+    }
     if posting_list.contains_key(&term) {
         let indices = &posting_list[&term];
         let index = indices.binary_search(&posting_index).unwrap();
-        if index-1 >= 0 {
+        if index-1 >= 0 as usize {
             return indices[index-1] as i32;
         }
     }
@@ -62,6 +80,16 @@ fn main() {
     println!("last : {}", result_last);
     println!("next : {}", result_next);
     println!("prev : {}", result_prev);
+
+    let result_next_inf = next("first".to_string(), INF, &posting_list);
+    let result_next_neg_inf = next("first".to_string(), NEG_INF, &posting_list);
+    let result_prev_inf = prev("first".to_string(), INF, &posting_list);
+    let result_prev_neg_inf = prev("first".to_string(), NEG_INF, &posting_list);
+    println!("");
+    println!("next inf    : {}", result_next_inf);
+    println!("next neg_inf: {}", result_next_neg_inf);
+    println!("prev inf    : {}", result_prev_inf);
+    println!("prev neg_inf: {}", result_prev_neg_inf);
 
     println!("DONE");
 }
