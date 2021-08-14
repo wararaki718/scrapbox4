72540f1ead56e18b7da8ba7ec94c4c79e18230b8
wararaki <ky7.ott.w@gmail.com>
2021-08-14 22:59:08 +0900

    update

--- nlp_sample/min_edit_distance/min_edit_distance/text.py
+++ nlp_sample/min_edit_distance/min_edit_distance/text.py
@@ -7,10 +7,10 @@ def min_edit_distance(source: str, target: str) -> int:
 
     D = np.zeros((n, m))
     for i in range(1, n):
-        D[i, 0] = D[i-1, 0] + 1
+        D[i, 0] = D[i-1, 0] + 1 # delete
     
     for i in range(1, m):
-        D[0, i] = D[0, i-1] + 1
+        D[0, i] = D[0, i-1] + 1 # insert
     
     for i in range(n-1):
         for j in range(m-1):
