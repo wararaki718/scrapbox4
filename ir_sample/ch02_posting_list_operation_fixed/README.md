a34b89bde75f4430f6edb225e391dae4dcac9fd0
wararaki <ky7.ott.w@gmail.com>
2021-09-06 20:32:34 +0900

    update

---
+++ ir_sample/ch02_posting_list_operation_fixed/README.md
@@ -0,0 +1,13 @@
+# posting list operations
+
+## build
+
+```shell
+cargo build
+```
+
+## run
+
+```shell
+cargo run
+```
---
+++ ir_sample/ch02_posting_list_operation_fixed/src/main.rs
@@ -0,0 +1,52 @@
+mod normalize;
+mod operations;
+mod posting;
+mod posting_list;
+
+use normalize::normalize;
+use operations::{docid, offset, first, last, next, prev};
+use posting::Posting;
+use posting_list::get_posting_list;
+
+
+fn main() {
+    let docs = vec![
+        "Do you quarrel, sir?",
+        "Quarrel sir! no, sir!",
+        "If you do, sir, I am for you: I serve as good a man as you.",
+        "No better.",
+        "Well, sir."
+    ];
+    let mut norm_docs = Vec::new();
+    for doc in docs {
+        norm_docs.push(normalize(String::from(doc)));
+    }
+    
+    // create posting list
+    let pl = get_posting_list(&norm_docs);
+
+    // check
+    println!("posting list:");
+    for (key, value) in &pl {
+        println!("{}: {:?}", key, value);
+    }
+    println!("");
+
+    println!("operations");
+    let position = Posting::new(1, 1);
+    let did = docid(position);
+    let off = offset(position);
+    println!("docid({:?}) : {:?}", position, did);
+    println!("offset({:?}): {:?}", position, off);
+
+    let term = "sir";
+    let fdoc = first(String::from(term), &pl);
+    let ldoc = last(String::from(term), &pl);
+    let ndoc = next(String::from(term), position, &pl);
+    let pdoc = prev(String::from(term), position, &pl);
+    println!("first({})  : {:?}", term, fdoc);
+    println!("last({})   : {:?}", term, ldoc);
+    println!("next({}:{:?}): {:?}", term, position, ndoc);
+    println!("prev({}:{:?}): {:?}", term, position, pdoc);
+    println!("DONE");
+}
