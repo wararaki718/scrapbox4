4bce3ac074d2b206716b976e1ec5c153d0f6484e
wararaki <ky7.ott.w@gmail.com>
2021-10-26 21:18:31 +0900

    update

--- ml_sample/sample_recommends/main.py
+++ ml_sample/sample_recommends/main.py
@@ -1,7 +1,7 @@
 from irspack.dataset.movielens import MovieLens1MDataManager
 
 from config import Config
-from movie_recommender import MovieKNNRecommender
+from movie_recommender import MovieKNNRecommender, MovieIALSRecommender
 
 
 def main():
@@ -15,11 +15,15 @@ def main():
 
     knn_recommender = MovieKNNRecommender(config.asymmetric_cosine_knn_config)
     knn_recommender.fit(df, user_ids, item_ids)
+    als_recommender = MovieIALSRecommender(config.ials_config)
+    als_recommender.fit(df, user_ids, item_ids)
     print("model trained")
     print("")
 
     print("get_recommend items:")
     print(knn_recommender.get_recommendations(1))
+    print("")
+    print(als_recommender.get_recommendations(1))
     
     print("DONE")
 
