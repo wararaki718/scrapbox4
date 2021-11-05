210cd00ac6ba61263889d04d35e190da3ebba80c
wararaki <ky7.ott.w@gmail.com>
2021-11-05 22:00:39 +0900

    update

---
+++ py_sample/sample_kubeflow_pipeline_local_docker/README.md
@@ -0,0 +1,19 @@
+# kubeflow pipeline use a local docker image
+
+## setup environment
+
+```shell
+pip install kfp
+```
+
+## build
+
+```shell
+docker-compose build
+```
+
+## create pipeline
+
+```shell
+python pipline.py
+```
---
+++ py_sample/sample_kubeflow_pipeline_local_docker/pipeline.py
@@ -0,0 +1,13 @@
+import kfp
+
+
+create_step_get_lines = kfp.components.load_component_from_file(
+    "components.yaml"
+)
+
+def pipeline():
+    get_line_step = (create_step_get_lines().set_image_pull_policy("Never"))
+
+
+if __name__ == "__main__":
+    kfp.compiler.Compiler().compile(pipeline, "sample_pipeline.yaml")
