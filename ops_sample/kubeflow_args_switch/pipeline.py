be3eae4bbd138f59be7f441938cfb67a1506e599
wararaki <ky7.ott.w@gmail.com>
2021-11-15 21:23:12 +0900

    update

--- ops_sample/kubeflow_args_switch/pipeline.py
+++ ops_sample/kubeflow_args_switch/pipeline.py
@@ -5,7 +5,7 @@ def _switch_op(sample_flag: str) -> kfp.dsl.ContainerOp:
     switch_op = kfp.components.load_component_from_file(
         "component.yaml"
     )
-    return (switch_op().set_image_pull_policy("Never"))
+    return (switch_op(sample_flag=sample_flag).set_image_pull_policy("Never"))
 
 
 def pipeline():
