6a037fc45e8fc5d5856a913a17c95687857a2660
wararaki <ky7.ott.w@gmail.com>
2021-12-25 21:57:06 +0900

    update

---
+++ nlp_sample/language_detection_based_on_fasttext/README.md
@@ -0,0 +1,19 @@
+# language detection based on fasttext
+
+## setup environment
+
+```shell
+pip install fasttext
+```
+
+## download model
+
+```shell
+wget -O /tmp/lid.176.bin https://dl.fbaipublicfiles.com/fasttext/supervised-models/lid.176.bin
+```
+
+## run
+
+```shell
+python main.py
+```
---
+++ nlp_sample/language_detection_based_on_fasttext/main.py
@@ -0,0 +1,22 @@
+import fasttext
+
+
+def main():
+    model_path = "/tmp/lid.176.bin"
+
+    model = fasttext.load_model(model_path)
+
+    sentences = [
+        "je mange de la nourriture",
+        "hello world",
+        "こんにちわ",
+        "hello こんにちわ"
+    ]
+
+    labels, _ = model.predict(sentences)
+    print(labels)
+    print("DONE")
+
+
+if __name__ == "__main__":
+    main()
