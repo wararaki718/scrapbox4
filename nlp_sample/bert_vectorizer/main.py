f3d00ac8535befc367e1c620d3fcfee1ef5e509c
wararaki <ky7.ott.w@gmail.com>
2022-01-30 21:37:55 +0900

    update

--- nlp_sample/bert_vectorizer/main.py
+++ nlp_sample/bert_vectorizer/main.py
@@ -1,10 +1,19 @@
+import torch
 from transformers import AutoTokenizer, AutoModel
+from typing import Union
+
+
+def try_gpu(x: Union[torch.Tensor, AutoModel]) -> Union[torch.Tensor, AutoModel]:
+    if torch.cuda.is_available():
+        return x.cuda()
+    else:
+        return x
 
 
 def main():
     model_name = "roberta-base"
     tokenizer = AutoTokenizer.from_pretrained(model_name)
-    model = AutoModel.from_pretrained(model_name)
+    model = try_gpu(AutoModel.from_pretrained(model_name))
     max_legnth = 128
 
     texts = [
@@ -20,7 +29,11 @@ def main():
         padding="max_length",
         return_tensors="pt"
     )
-    out = model(**tokens)
+    print(tokens.keys())
+    out = model(
+        input_ids=try_gpu(tokens["input_ids"]),
+        attention_mask=try_gpu(tokens["attention_mask"])
+    )
     print(type(out))
 
     result = out.last_hidden_state.mean(1).detach().cpu().numpy()
