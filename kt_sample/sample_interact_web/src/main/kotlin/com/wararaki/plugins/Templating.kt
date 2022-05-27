e1ad1c646d704b16e5afdeb9e6b4a2b85bb90471
wararaki <ky7.ott.w@gmail.com>
2022-05-27 23:45:09 +0900

    update

---
+++ kt_sample/sample_interact_web/src/main/kotlin/com/wararaki/plugins/Templating.kt
@@ -0,0 +1,23 @@
+package com.wararaki.plugins
+
+import freemarker.cache.*
+import freemarker.core.HTMLOutputFormat
+import io.ktor.server.freemarker.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.request.*
+import io.ktor.server.routing.*
+
+fun Application.configureTemplating() {
+    install(FreeMarker) {
+        templateLoader = ClassTemplateLoader(this::class.java.classLoader, "templates")
+        outputFormat = HTMLOutputFormat.INSTANCE
+    }
+
+    routing {
+        get("/html-freemarker") {
+            call.respond(FreeMarkerContent("index.ftl", mapOf("data" to IndexData(listOf(1, 2, 3))), ""))
+        }
+    }
+}
+data class IndexData(val items: List<Int>)
